ead0fdca043b5af1a006a3b92d773492e78caa51
Jon-INJC <chalachewyonathan8@gmail.com>
2025-10-23 11:53:01 +0300

    Changed the instanse

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 public class Main{
     public static void main(String[] args) {
-        //Choice choice = new Choice();
+        Choice choice = new Choice();
         //MainChoice mainChoice = new MainChoice();
         //NewProduct newProduct = new NewProduct();
         //Login login = new Login();
         //SeeTable seeTable = new SeeTable();
         //Signin signin = new Signin();
-        AddToStock addToStock = new AddToStock();
+        //AddToStock addToStock = new AddToStock();
         //SearchProduct searchProduct = new SearchProduct();
         //ProductInfo productInfo = new ProductInfo("12345");
         //SubFromStock subFromStock = new SubFromStock();
